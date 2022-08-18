3500b71224baab66fb898de21bef7e1521d64751
Olha <99271539+Olha-Khr@users.noreply.github.com>
2022-08-19 02:34:29 +0300

    js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
+     return new Promise((resolve, reject) => {
   if (shouldResolve) {
+      resolve({
+        position: position,
+        delay: delay,
+      });
     // Fulfill
   } else {
-    // Reject
+      reject({
+        position: position,
+        delay: delay,
+      });
+    }
+  });
+}
+const form = document.querySelector('.form');
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  let {
+    elements: { delay, step, amount },
+  } = e.target;
+
+  const delayValue = Number(delay.value);
+  const stepValue = Number(step.value);
+
+  for (let i = 1, delay = delayValue; i <= amount.value; i += 1, delay += stepValue) {
+    setTimeout(() => {
+      createPromise(i, delay)
+        .then(({ position, delay }) => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }, delay);
   }
 }
